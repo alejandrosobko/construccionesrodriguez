0677cc29660058ed02426011166ed3caa5fc66e2
Alejandro Sobko <alee-sk8@hotmail.com>
2018-06-09 17:39:42 -0300

    Add scroll to top

--- js/functions.js
+++ js/functions.js
@@ -10,3 +10,8 @@ $(document).on('click', '.nav li', function (event) {
 
     $('#nav-check').prop('checked', false);
 });
+
+$(".logo").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
