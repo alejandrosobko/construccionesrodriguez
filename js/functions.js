860bf57d6d00f9d4e7db5915e2ceab7e65561bd8
Alejandro Sobko <alee-sk8@hotmail.com>
2018-07-01 17:03:58 -0300

    Add details of services

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,22 @@
-$(document).on('click', '.nav li', function (event) {
-    event.preventDefault();
-
-    var link = $(event.currentTarget).find('a');
+var goToSection = function(link) {
     var href = link.attr('href');
 
     $('html, body').animate({
         scrollTop: $(href).offset().top - 55
     }, 500);
+}
 
+$(document).on('click', '.nav li', function (event) {
+    event.preventDefault();
+    var link = $(event.currentTarget).find('a');
+    goToSection(link);
     $('#nav-check').prop('checked', false);
+});
+
+$(document).on('click', 'section#services a', function (event) {
+    event.preventDefault();
+    var link = $(event.currentTarget);
+    goToSection(link);
 });
 
 $(".logo").click(function() {
