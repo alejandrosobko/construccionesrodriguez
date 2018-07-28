d3af2af1db097231e8fba2aa872bff5c6b7e7fb7
Alejandro Sobko <alee-sk8@hotmail.com>
2018-07-28 16:14:51 -0300

    Fix height and add functions

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,8 @@ var goToSection = function(link) {
     }, 500);
 }
 
+
+// go to section on click navbar item
 $(document).on('click', '.nav li', function (event) {
     event.preventDefault();
     var link = $(event.currentTarget).find('a');
@@ -13,22 +15,49 @@ $(document).on('click', '.nav li', function (event) {
     $('#nav-check').prop('checked', false);
 });
 
+
+// go to specific section
 $(document).on('click', 'section#services a', function (event) {
     event.preventDefault();
     var link = $(event.currentTarget);
     goToSection(link);
 });
 
+
+// go to top on click logo
 $(".logo").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
 
 
+// open carousel in a modal on click an image
 $(function() {
     $('.pop').on('click', function(event) {
         event.preventDefault();
-        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        let $images = $(event.currentTarget).parents('.carousel-inner').find('img');
+        $('.modal-content').find('img').each(function(index, img) {
+            img.src = $images[index].src;
+        });
+
         $('#imagemodal').modal('show');
     });
 });
+
+
+// handle key press
+$(document).keydown(function(e) {
+    if (e.keyCode === 37) {
+       // Previous
+       $(".carousel-control.left").click();
+       return false;
+    }
+    if (e.keyCode === 39) {
+       // Next
+       $(".carousel-control.right").click();
+       return false;
+    }
+});
+
+
+// handle swipe on mobile screen
